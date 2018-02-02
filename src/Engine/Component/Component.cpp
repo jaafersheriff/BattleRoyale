b0f2158158bcb3489203fd757ca1f823cb6efc86
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-02-01 22:57:05 -0800

    Finished component interface

---
+++ src/Engine/Component/Component.cpp
@@ -0,0 +1,19 @@
+#include "Component.hpp"
+
+#include "GameObject/GameObject.hpp"
+
+Component::Component() {
+
+}
+
+void Component::init() {
+
+}
+
+void Component::update(float dt) {
+
+}
+
+void Component::receiveMessage(Message *msg) {
+
+}
--- src/Engine/Component/Component.hpp
+++ src/Engine/Component/Component.hpp
@@ -2,8 +2,28 @@
 #ifndef _COMPONENT_HPP_
 #define _COMPONENT_HPP_
 
+#include "GameObject/Message.hpp"
+
+class GameObject;
 class Component {
-    // TODO
+    public:
+
+        Component();
+        virtual void init();
+        virtual void update(float);
+
+        GameObject* getGameObject() { return gameObject;  }
+        void setGameObject(GameObject *go) { this->getGameObject = go; }
+
+        /* Receive a message sent by another component */
+        virtual void receiveMessage(Message *);
+
+        /* Denotes if component should be removed */
+        bool isTerminated;
+    private:
+
+        /* Parent game object */
+        GameObject* gameObject;
 };
 
 #endif
