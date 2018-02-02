d8c4b5b01606254643784622812da35dbfd8a08b
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-02-02 00:54:19 -0800

    Camera Controller component

---
+++ src/Engine/Component/CameraComponents/CameraController.cpp
@@ -0,0 +1,64 @@
+#include "CameraController.hpp"
+
+#include "IO/Mouse.hpp"
+#include "IO/Keyboard.hpp"
+
+void CameraController::init() {
+
+}
+
+void CameraController::update(float dt) {
+    if (Mouse::isDown(1)) {
+        lookAround(dt, Mouse::dx, Mouse::dy);
+    }
+
+    if (Keyboard::isKeyPressed(front)) 
+        moveFront(dt);
+    if (Keyboard::isKeyPressed(back)) 
+        moveBack(dt);
+    if (Keyboard::isKeyPressed(left)) 
+        moveLeft(dt);
+    if (Keyboard::isKeyPressed(right)) 
+        moveRight(dt);
+    if (Keyboard::isKeyPressed(up)) 
+        moveUp(dt);
+    if (Keyboard::isKeyPressed(down)) 
+        moveDown(dt);
+
+}
+
+void CameraController::lookAround(float dt, float dx, float dy) {
+    camera->theta += dx * lookSpeed * dt;
+    camera->phi -= dy * lookSpeed * dt;
+}
+
+void CameraController::moveFront(float dt) {
+    camera->position += camera->w * moveSpeed * dt;
+    camera->lookAt += camera->w * moveSpeed * dt;
+}
+
+void CameraController::moveBack(float dt) {
+    camera->position -= camera->w * moveSpeed * dt;
+    camera->lookAt -= camera->w * moveSpeed * dt;
+}
+
+void CameraController::moveRight(float dt) {
+    camera->position += camera->u * moveSpeed * dt;
+    camera->lookAt += camera->u * moveSpeed * dt;
+}
+
+void CameraController::moveLeft(float dt) {
+    camera->position -= camera->u * moveSpeed * dt;
+    camera->lookAt -= camera->u * moveSpeed * dt;
+}
+
+void CameraController::moveUp(float dt) {
+    camera->position += camera->v * moveSpeed * dt;
+    camera->lookAt += camera->v * moveSpeed * dt;
+}
+
+void CameraController::moveDown(float dt) {
+    camera->position -= camera->v * moveSpeed * dt;
+    camera->lookAt -= camera->v * moveSpeed * dt;
+}
+
