77527626c75e8c8ee8772b37aa6af1478cc1c954
Jaafer Sheriff <jaafersheriff@gmail.com>
2018-02-02 17:35:43 -0800

    Added print vector to util TODO : Logger

--- src/Engine/Util/Util.hpp
+++ src/Engine/Util/Util.hpp
@@ -3,9 +3,16 @@
 #ifndef _UTIL_HPP_
 #define _UTIL_HPP_
 
+#include <string>
+#include <iostream>
+
 struct Util {
     static constexpr double PI = 3.14159265359;
 
+    static inline void printVector(std::string name, glm::vec3 vec) {
+        std::cout << name << ": <" <<
+            vec.x << ", " << vec.y << " " << vec.z << ">" << std::endl;
+    }
 };
 
 #endif
