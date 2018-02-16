fed76fb2f815bf167cade5e32c15baa1cdb606c1
Austin Quick <daskie.q@gmail.com>
2018-02-15 18:42:26 -0800

    fix

--- src/Engine/Component/CameraComponents/CameraController.cpp
+++ src/Engine/Component/CameraComponents/CameraController.cpp
@@ -32,3 +32,38 @@ void CameraController::update(float dt) {
     dir = camera->u() * uw.x + glm::vec3(0.0f, 1.0f, 0.0f) * dir.y +  camera->w() * uw.y; // WoW controls
     gameObject->getSpatial()->move(dir * moveSpeed * dt);
 }
+void CameraController::lookAround(float dt) {
+    camera->angle(float(Mouse::dx) * lookSpeed * dt, -float(Mouse::dy) * lookSpeed * dt, true);
+}
+
+void CameraController::moveFront(float dt) {
+    gameObject->getSpatial()->move(-camera->w() * moveSpeed * dt);
+}
+
+void CameraController::strafeForward(float dt) {
+    gameObject->getSpatial()->move(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), camera->u()) * moveSpeed * dt);
+}
+
+void CameraController::moveBack(float dt) {
+    gameObject->getSpatial()->move(camera->w() * moveSpeed * dt);
+}
+
+void CameraController::strafeBackward(float dt) {    
+    gameObject->getSpatial()->move(glm::cross(camera->u(), glm::vec3(0.0f, 1.0f, 0.0f)) * moveSpeed * dt);
+}
+
+void CameraController::moveRight(float dt) {
+    gameObject->getSpatial()->move(camera->u() * moveSpeed * dt);
+}
+
+void CameraController::moveLeft(float dt) {
+    gameObject->getSpatial()->move(-camera->u() * moveSpeed * dt);
+}
+
+void CameraController::moveUp(float dt) {
+    gameObject->getSpatial()->move(glm::vec3(0.0f, 1.0f, 0.0f) * moveSpeed * dt);
+}
+
+void CameraController::moveDown(float dt) {
+    gameObject->getSpatial()->move(glm::vec3(0.0f, -1.0f, 0.0f) * moveSpeed * dt);
+}
