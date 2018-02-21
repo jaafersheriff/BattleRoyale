a1eaf681de7a29673e749f7d953425c953b5625c
Austin Quick <daskie.q@gmail.com>
2018-02-20 19:48:25 -0800

    incorporated changes from jaafer's pr

--- src/Engine/System/RenderSystem.cpp
+++ src/Engine/System/RenderSystem.cpp
@@ -23,9 +23,6 @@ void RenderSystem::init() {
 }
 
 void RenderSystem::update(float dt) {
-    /* Update components */
-    System::update(dt);
-
     /* Reset rendering display */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.2f, 0.3f, 0.4f, 1.f);
