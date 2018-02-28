535a2c50fe74deefad8f174a0efa064fdf5eacb2
Zachary Hatton <zachattackhatton@gmail.com>
2018-02-28 14:31:59 -0800

    3D sound base

--- src/Engine/System/SoundSystem.cpp
+++ src/Engine/System/SoundSystem.cpp
@@ -5,6 +5,7 @@ String SoundSystem::s_SOUND_DIR = EngineApp::RESOURCE_DIR + "/soundeffects/";
 #ifdef HAVE_FMOD_LIBRARY
 FMOD::System* SoundSystem::s_system = NULL;
 Map<String, FMOD::Sound*> SoundSystem::s_soundLibrary = Map<String, FMOD::Sound*>();
+Listener SoundSystem::s_player = Listener();
 #endif
 void SoundSystem::init() {
 
@@ -39,8 +40,10 @@ void SoundSystem::initSoundLibrary()
 {   
     Vector<String> sfn = getSoundFilenames("filesinfolder.txt");
     for (auto &s : sfn) {
-        FMOD::Sound* tempSound = createSound(s);
-        s_soundLibrary[s] = tempSound;
+        FMOD::Sound* tempSound = createSound(s, FMOD_DEFAULT);
+        s_soundLibrary[s + "2D"] = tempSound;
+        tempSound = createSound(s, FMOD_3D);
+        s_soundLibrary[s + "3D"] = tempSound;
     }
 }
 
@@ -69,7 +72,21 @@ Vector<String> SoundSystem::getSoundFilenames(String listname) {
     return soundfilenames;
 }
 
-FMOD::Sound* SoundSystem::createSound(String soundfilename) 
+void SoundSystem::updateListener(glm::vec3 pos, glm::vec3 vel, glm::vec3 forward, glm::vec3 up) {
+    s_player.id = 0;
+    setFVec(s_player.pos, pos);
+    setFVec(s_player.vel, vel);
+    setFVec(s_player.forward, forward);
+    setFVec(s_player.up, up);
+}
+
+void SoundSystem::setFVec(FMOD_VECTOR *v, glm::vec3 g) {
+    v->x = g.x;
+    v->y = g.y;
+    v->z = g.z;
+}
+
+FMOD::Sound* SoundSystem::createSound(String soundfilename, FMOD_MODE m) 
 {
     String fullpath = s_SOUND_DIR + soundfilename;
     const char* path = fullpath.c_str();	
@@ -86,16 +103,37 @@ FMOD::Sound* SoundSystem::createSound(String soundfilename)
 //play sound from resources/soundeffects by filename
 void  SoundSystem::playSound(String fileName) {
     FMOD::Sound *sound;
-    if (s_soundLibrary.count(fileName)) {
-        sound = s_soundLibrary[fileName];
+    if (s_soundLibrary.count(fileName + "2D")) {
+        sound = s_soundLibrary[fileName + "2D"];
     }
     else {
-        sound = createSound(fileName);
+        sound = createSound(fileName + "2D", FMOD_DEFAULT);
     }
     FMOD_RESULT result = s_system->playSound(sound, NULL, false, NULL);
     if (result != FMOD_OK) {
         printf("playSound() done goofed!\n");
     }
-    
+}
+
+void SoundSystem::playSound3D(String fileName, glm::vec3 pos) {
+    FMOD::Sound *sound;
+    FMOD::Channel *newChannel;
+    FMOD_VECTOR fPos;
+
+    if (s_soundLibrary.count(fileName + "3D")) {
+        sound = s_soundLibrary[fileName + "3D"];
+    }
+    else {
+        sound = createSound(fileName + "3D", FMOD_3D);
+    }
+
+    setFVec(&fPos, pos);
+    FMOD_RESULT result = s_system->playSound(sound, NULL, false, &newChannel);
+    newChannel->setPaused(true);
+    newChannel->set3DAttributes(&fPos, NULL, NULL);
+    newChannel->setPaused(false);
+    if (result != FMOD_OK) {
+        printf("playSound() done goofed!\n");
+    }
 }
 #endif
--- src/Engine/System/SoundSystem.hpp
+++ src/Engine/System/SoundSystem.hpp
@@ -5,11 +5,20 @@
 #include "System.hpp"
 #include "Component/SoundComponents/SoundComponent.hpp"
 #include "Scene/Scene.hpp"
+
 #include "EngineApp/EngineApp.hpp"
 
 #ifdef HAVE_FMOD_LIBRARY 
 #include <fmod.hpp>
 #include <fmod_studio.hpp>
+
+typedef struct {
+    int id;
+    FMOD_VECTOR *pos = new FMOD_VECTOR();
+    FMOD_VECTOR *vel = new FMOD_VECTOR();
+    FMOD_VECTOR *forward = new FMOD_VECTOR();
+    FMOD_VECTOR *up = new FMOD_VECTOR();
+} Listener;
 #endif
 
 class SoundComponent;
@@ -31,6 +40,7 @@ class SoundSystem {
         static String s_SOUND_DIR;
         static const Vector<SoundComponent *> & s_soundComponents;
     #ifdef HAVE_FMOD_LIBRARY
+        static Listener s_player;
         static Map<String, FMOD::Sound*> s_soundLibrary;
     #endif
 
@@ -41,6 +51,7 @@ class SoundSystem {
 
     #ifdef HAVE_FMOD_LIBRARY
         static void  playSound(String name);
+        static void playSound3D(String name, glm::vec3 pos);
     #endif
 
 	private:
@@ -49,8 +60,10 @@ class SoundSystem {
     
     #ifdef HAVE_FMOD_LIBRARY
         static Vector<String> getSoundFilenames(String listname);
-        static FMOD::Sound* createSound(String soundfilename);
+        static FMOD::Sound* createSound(String soundfilename, FMOD_MODE m);
         static void initSoundLibrary();
+        static void updateListener(glm::vec3 pos, glm::vec3 vel, glm::vec3 forward, glm::vec3 up);
+        static void setFVec(FMOD_VECTOR *v, glm::vec3 g);
     #endif
 };  
 #endif
